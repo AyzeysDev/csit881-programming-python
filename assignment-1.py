eec54f074c10fdf3ab4e2d4c31232a266cbafc41
Aravind Kumar <ara3194@gmail.com>
2023-03-15 09:14:06 +1100

    Assignment 1 initial commit

---
+++ assignment-1.py
@@ -0,0 +1,10 @@
+#################
+
+#Answer 1
+frog_lifecycle = input("Enter the number of steps from egg to frog: ")
+arrow = "-->" * int(frog_lifecycle)
+print("Here is the expected life cycle:")
+print("{0}{1}{2}".format("egg", arrow, "frog"))
+
+######################
+#Answer 2
